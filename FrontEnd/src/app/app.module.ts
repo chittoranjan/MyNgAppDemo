3a72a20be4100247ce59ebdaa30b67952f79001f
chittoranjan <chittoranjan.sarker@gmail.com>
2020-02-03 02:51:08 +0600

    employee Create Form Added

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -1,20 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Route, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ListEmployeesComponent } from './employees/list-employees.component';
+import { CreateEmployeeComponent } from './employees/create-employee.component';
+import {FormsModule} from '@angular/forms';
+
+
+const appRoutes: Routes = [
+{ path: 'list', component: ListEmployeesComponent},
+{ path: 'create', component: CreateEmployeeComponent},
+{ path: '', redirectTo: '/list', pathMatch: 'full'}
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListEmployeesComponent
+    ListEmployeesComponent,
+    CreateEmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
