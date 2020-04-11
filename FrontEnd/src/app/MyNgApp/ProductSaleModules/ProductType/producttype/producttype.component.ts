160c3b9c6651071355ddfe6d1fd1a5d59916de08
chittoranjan <chittoranjan.sarker@gmail.com>
2020-04-12 00:45:41 +0600

    form validation updated

--- FrontEnd/src/app/MyNgApp/ProductSaleModules/ProductType/producttype/producttype.component.ts
+++ FrontEnd/src/app/MyNgApp/ProductSaleModules/ProductType/producttype/producttype.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, Validators } from '@angular/forms';
+import { FormGroup, Validators, EmailValidator } from '@angular/forms';
 import { ProducttypeService } from '../producttype.service';
-import { ProductType } from '../product-type.model';
 import { FormBuilder } from '@angular/forms';
+import { ProductType } from '../product-type.model';
 
 @Component({
   selector: 'app-producttype',
@@ -15,20 +15,24 @@ export class ProducttypeComponent implements OnInit {
 
   validationMessages = {
     name: {
-      required: 'Name is Required.',
+      required: 'Name is required.',
       minlength: 'Name must be greater then 2 characters.',
       maxlength: 'Name must be less then 100 characters.'
     },
     description: {
-      required: 'Description is Required.',
+      required: 'Description is required.',
       minlength: 'Description must be greater then 2 characters.',
       maxlength: 'Description must be less then 200 characters.'
+    },
+    email: {
+      required: 'Email address is required.'
     }
   };
 
   formErrors = {
     name: '',
-    description: ''
+    description: '',
+    email: '',
   };
 
   constructor(
@@ -63,7 +67,7 @@ export class ProducttypeComponent implements OnInit {
         this.logValidationErrors(abstractControl);
       } else {
         this.formErrors[key] = '';
-        if (abstractControl && abstractControl.valid) {
+        if (abstractControl && !abstractControl.valid && (abstractControl.touched || abstractControl.dirty)) {
           const messages = this.validationMessages[key];
           for (const errorKey in abstractControl.errors) {
             if (errorKey) {
@@ -75,9 +79,16 @@ export class ProducttypeComponent implements OnInit {
     });
   }
 
-  save(model) {
-    this._service.save(model).subscribe(res => {
-      this.productTypeFormInstance();
-    });
+  save(form) {
+
+    if (form.valid) {
+      const model = form.value as ProductType;
+      this._service.save(model).subscribe(res => {
+        this.productTypeFormInstance();
+      });
+    } else {
+      this.logValidationErrors(this.productTypeForm); // how to make all are touched?
+    }
+
   }
 }
