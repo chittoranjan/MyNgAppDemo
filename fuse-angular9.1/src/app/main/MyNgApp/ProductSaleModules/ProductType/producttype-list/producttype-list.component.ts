a3d6bc3787a7c2354ce7d07d1b3422d107559ddf
chittoranjan <chittoranjan.sarker@gmail.com>
2020-05-26 22:04:29 +0600

    Table UI align

--- fuse-angular9.1/src/app/main/MyNgApp/ProductSaleModules/ProductType/producttype-list/producttype-list.component.ts
+++ fuse-angular9.1/src/app/main/MyNgApp/ProductSaleModules/ProductType/producttype-list/producttype-list.component.ts
@@ -1,18 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ProductType } from '../product-type.model';
 import { ProducttypeService } from '../producttype.service';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { fuseAnimations } from '@fuse/animations';
 
 @Component({
   selector: 'app-producttype-list',
   templateUrl: './producttype-list.component.html',
-  styleUrls: ['./producttype-list.component.scss']
+  styleUrls: ['./producttype-list.component.scss'],
+  animations: fuseAnimations,
 })
 export class ProducttypeListComponent implements OnInit {
 
   productTypeList: ProductType[] = [];
   dataSource: MatTableDataSource<ProductType>;
   displayedColumns = ['id', 'name', 'description', 'action'];
+
+  @ViewChild(MatPaginator, { static: true })
+  paginator: MatPaginator;
+
+  @ViewChild('filter', { static: true })
+  filter: ElementRef;
+
+  @ViewChild(MatSort, { static: true })
+  sort: MatSort;
 
   constructor(
     private _service: ProducttypeService,
--- fuse-angular9.1/src/app/main/MyNgApp/ProductSaleModules/product/product-list/product-list.component.ts
+++ fuse-angular9.1/src/app/main/MyNgApp/ProductSaleModules/product/product-list/product-list.component.ts
@@ -1,18 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Product } from '../product.modle';
 import { MatTableDataSource } from '@angular/material/table';
 import { ProductService } from '../product.service';
+import { fuseAnimations } from '@fuse/animations';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.scss']
+  styleUrls: ['./product-list.component.scss'],
+  animations: fuseAnimations,
 })
 export class ProductListComponent implements OnInit {
 
   productList: Product[] = [];
   dataSource: MatTableDataSource<Product>;
   displayedColumns = ['id', 'name', 'price', 'productType', 'description', 'action'];
+
+  @ViewChild(MatPaginator, { static: true })
+  paginator: MatPaginator;
+
+  @ViewChild('filter', { static: true })
+  filter: ElementRef;
+
+  @ViewChild(MatSort, { static: true })
+  sort: MatSort;
 
   constructor(
     private _service: ProductService,
